b358519a7aada1e45d93ac0bed471b3c6b1e291a
Danilo Ribeiro <danilo.yuri.ribeiro@gmail.com>
2023-05-15 20:50:04 -0300

    primeira versão rating

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+let selected = null;
+
+document.querySelectorAll(".list-item").forEach(item => {
+    item.addEventListener('click', () => {
+        document.querySelectorAll('.select').forEach(input => {
+            input.classList.remove('select')
+        })
+        item.classList.add('select')
+        selected = item.innerHTML
+    })
+})
+
+const btn = document.querySelector("button").addEventListener("click", (e) => {
+    e.preventDefault()
+    if (selected != null) {
+        let primeiroDisplay = document.querySelector(".start-rate")
+        let segundoDisplay = document.querySelector(".end-rate")
+        let selection = document.querySelector(".selection")
+
+        selection.innerHTML = `<p>You selected ${selected} out of 5</p>`
+
+        primeiroDisplay.classList.add("none")
+        segundoDisplay.classList.remove("none")
+    } else {
+        alert("You need to select at least 1 grade!!")
+    }
+
+})
